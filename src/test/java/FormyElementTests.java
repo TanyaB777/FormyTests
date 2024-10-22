a13b931b50bf2a72849375f2b8997cdd2c7c7c62
tanya_bilka <tanyawbw7@gmail.com>
2024-10-22 23:17:20 +0300

    Empty line was deleted

--- src/test/java/FormyElementTests.java
+++ src/test/java/FormyElementTests.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import java.time.Duration;
-
 import static org.testng.Assert.*;
 
 public class FormyElementTests extends BaseTest {
